fe7c5eeb23a5b0c5f6ed50c7bf0fd1d480118fb7
Gabriel Landry <gablandry31@gmail.com>
2025-02-08 16:53:19 -0500

    Mise en place de l'authentification

--- backend/start/routes.ts
+++ backend/start/routes.ts
@@ -7,10 +7,14 @@
 |
 */
 
+import AuthController from '#controllers/auth_controller'
 import router from '@adonisjs/core/services/router'
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
+router.group(() => {
+  router.group(() => {
+    router.get('/me', [AuthController, 'me'])
+    router.delete('/logout', [AuthController, 'logout'])
+    router.post('/register', [AuthController, 'register'])
+    router.post('/login', [AuthController, 'login'])
+  }).prefix('/auth')
+}).prefix('/api')
