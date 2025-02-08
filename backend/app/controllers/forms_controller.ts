c925b53499d2645ae13e74d9324738ec352fdfd5
Gabriel Landry <gablandry31@gmail.com>
2025-02-08 17:15:20 -0500

    Fix du form controller

--- backend/app/controllers/forms_controller.ts
+++ backend/app/controllers/forms_controller.ts
@@ -1,27 +1,27 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http';
 
 export default class FormsController {
 
-    async submitForm({ request, response }: HttpContext) {
+  async submitForm({ }: HttpContext) {
 
-        
-        // const data = request.only(['token'])
-        // const validatedUserData = await VerifyEmailValidator.validate(data)
-        // const jwtToken = jwt.verify(validatedUserData.token, process.env.JWT_SECRET as string) as {
-        //   email: string
-        //   verificationToken: string
-        // }
-        // const user = await User.findBy('email', jwtToken.email)
-        // if (!user) return response.notFound({ message: 'User not found' })
-        // if (user.verified) return response.badRequest({ message: 'Email already verified' })
-        // if (user.verificationToken !== jwtToken.verificationToken)
-        //   return response.badRequest({ message: 'Invalid verification token' })
-        // if (user.verificationTokenExpires && user.verificationTokenExpires < DateTime.now())
-        //   return response.badRequest({ message: 'Verification token expired' })
-        // user.verificationToken = null
-        // user.verified = true
-        // user.verificationTokenExpires = null
-        // await user.save()
-        // return response.ok(user)
-      }
+
+    // const data = request.only(['token'])
+    // const validatedUserData = await VerifyEmailValidator.validate(data)
+    // const jwtToken = jwt.verify(validatedUserData.token, process.env.JWT_SECRET as string) as {
+    //   email: string
+    //   verificationToken: string
+    // }
+    // const user = await User.findBy('email', jwtToken.email)
+    // if (!user) return response.notFound({ message: 'User not found' })
+    // if (user.verified) return response.badRequest({ message: 'Email already verified' })
+    // if (user.verificationToken !== jwtToken.verificationToken)
+    //   return response.badRequest({ message: 'Invalid verification token' })
+    // if (user.verificationTokenExpires && user.verificationTokenExpires < DateTime.now())
+    //   return response.badRequest({ message: 'Verification token expired' })
+    // user.verificationToken = null
+    // user.verified = true
+    // user.verificationTokenExpires = null
+    // await user.save()
+    // return response.ok(user)
+  }
 }
