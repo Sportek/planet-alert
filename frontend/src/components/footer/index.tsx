e6dc8d846b9fcab7b834fb9e7b36d812ff07bc1f
Gabriel <daniuriza@outlook.com>
2025-02-09 03:14:28 -0500

    footer

--- frontend/src/components/footer/index.tsx
+++ frontend/src/components/footer/index.tsx
@@ -8,23 +8,31 @@ import { Button } from "../ui/button";
 const Footer = ({ className }: { className?: string }) => {
 
   return (
-    <div className={cn("flex flex-col items-center gap-4 py-4 bg-emerald-600 text-white", className)}>
-
+    <div
+      className={cn(
+        "flex flex-col items-center gap-4 py-4 bg-emerald-600 text-white",
+        className
+      )}
+    >
       <RestrictedWidthLayout className="gap-4 flex flex-col items-center justify-center">
         <div className="flex flex-col md:grid md:grid-cols-3 gap-4">
           <div className="flex flex-col items-center gap-4">
             <div className="flex flex-row items-center justify-start w-full gap-2">
-              <Image src={PlanetAlertLogo} alt="Planet Alert Logo" width={32} height={32} />
+              <Image
+                src={PlanetAlertLogo}
+                alt="Planet Alert Logo"
+                width={32}
+                height={32}
+              />
               <div className="text-2xl font-bold">Planète Alerte</div>
             </div>
             <div className="flex flex-col items-center gap-2">
-
-
               <div className="text-sm">
-                Planète Alerte est une plateforme interactive qui permet aux citoyens de signaler incidents environnementaux et urgences locales sur une carte.
+                Planète Alerte est une plateforme interactive qui permet aux
+                citoyens de signaler incidents environnementaux et urgences
+                locales sur une carte.
               </div>
               <Link href="/account/add-server" className="w-full">
-
                 <Button className="w-full">Ajouter un signalement</Button>
               </Link>
             </div>
@@ -39,53 +47,29 @@ const Footer = ({ className }: { className?: string }) => {
               >
                 GitHub
               </Link>
-
-
-              <Link
-                href={`${process.env.NEXT_PUBLIC_API_URL}/docs`}
-                className="text-white hover:text-zinc-100"
-              >
-                API Documentation
-              </Link>
-
+              {/* <Link href="/" className="text-white hover:text-zinc-100"> */}
+              API Documentation
+              {/* </Link> */}
             </div>
           </div>
           <div className="flex flex-col gap-4">
             <div className="text-md font-bold">AUTRES</div>
             <div className="flex flex-col gap-2 text-sm *:py-1">
-              <Link
-                href="/cgu"
-                className="text-white hover:text-zinc-100"
-              >
-                Conditions générales d'utilisation
-              </Link>
-
-
-              <Link
-                href="/login"
-
-                className="text-white hover:text-zinc-100"
-              >
+              {/* <Link href="/" className="text-white hover:text-zinc-100"> */}
+              Conditions générales d'utilisation
+              {/* </Link> */}
+              <Link href="/login" className="text-white hover:text-zinc-100">
                 Connexion
-
-
               </Link>
-              <Link
-                href="/register"
-
-                className="text-white hover:text-zinc-100"
-              >
+              <Link href="/register" className="text-white hover:text-zinc-100">
                 Inscription
-
               </Link>
             </div>
-
           </div>
         </div>
         <hr className="w-full h-[2px] bg-white" />
         <div className="flex flex-row items-center justify-center w-full">
           <div className="text-xs text-white">
-
             <div className="flex flex-col items-center justify-center">
               <div>
                 Copyright © 2025 Planète Alerte. Tous les droits sont réservés.
